fd874ae63dd21b2ef4dc5acd5052468141df2e83
Liam Percy <liampercy123@gmail.com>
2021-03-09 22:22:49 +1000

    Problem 1 complete

--- Week 1/Problem 1/Problem1.py
+++ Week 1/Problem 1/Problem1.py
@@ -1,12 +1,15 @@
+from datetime import datetime, date
 import pandas as pd
+import itertools
+
 
 def create_date(row):
-        return pd.to_datetime(row.Year*10000+row.Month*100+row.Day,format='%Y%m%d')
+         return datetime.strptime('{:04d}/{:02d}/{:02d}'.format(row.Year, row.Month, row.Day), '%Y/%m/%d')
 
 def load_bom(): 
-    rain = pd.read_csv('BOM\\IDCJAC0009_040913_1800_Data.csv')
-    temp = pd.read_csv('BOM\\IDCJAC0010_040913_1800_Data.csv')
-    solar = pd.read_csv('BOM\\IDCJAC0016_040913_1800_Data.csv')
+    rain = pd.read_csv('Week 1\\Problem 1\\BOM\\IDCJAC0009_040913_1800_Data.csv')
+    temp = pd.read_csv('Week 1\\Problem 1\\BOM\\IDCJAC0010_040913_1800_Data.csv')
+    solar = pd.read_csv('Week 1\\Problem 1\\BOM\\IDCJAC0016_040913_1800_Data.csv')
 
     rain = rain[(rain.Year >= 2014) & (rain.Year <= 2018)]
     temp = temp[(temp.Year >= 2014) & (temp.Year <= 2018)]
@@ -16,30 +19,77 @@ def load_bom():
     temp['Date'] = temp.apply(create_date, axis=1)
     solar['Date'] = solar.apply(create_date, axis=1)
 
-    combined = pd.merge(rain, temp, on="Date")
-    combined = pd.merge(combined, solar, on='Date')
+    bom = pd.merge(rain, temp, on="Date")
+    bom = pd.merge(bom, solar, on='Date')
 
-    combined = combined.drop(['Year', 'Month', 'Day', 'Year_x', 'Month_x', 'Day_x', 'Year_y', 'Month_y', 'Day_y'], axis=1)
+    bom = bom.drop(
+        ['Year', 'Month', 'Day', 
+        'Year_x', 'Month_x', 'Day_x', 
+        'Year_y', 'Month_y', 'Day_y', 
+        'Quality_x', 'Quality_y', 
+        'Product code', 'Product code_x', 'Product code_y',
+        'Bureau of Meteorology station number',
+        'Bureau of Meteorology station number_x', 
+        'Bureau of Meteorology station number_y',
+        'Period over which rainfall was measured (days)',
+        'Days of accumulation of maximum temperature'],
+        axis=1)
 
-    combined.to_csv('BOM_data.csv')
+    bom.to_csv('BOM_data.csv')
 
-    return combined
+    return bom
 
 
 def load_bcc():
-    bcc2014 = pd.read_csv('Week 1\\BCCCyclewayCounts\\bike-ped-auto-counts-2014.csv')
+    bcc2014 = pd.read_csv('Week 1\\Problem 1\\BCCCyclewayCounts\\bike-ped-auto-counts-2014.csv')
+    bcc2015 = pd.read_csv('Week 1\\Problem 1\\BCCCyclewayCounts\\bike-ped-auto-counts-2015.csv')
+    bcc2016 = pd.read_csv('Week 1\\Problem 1\\BCCCyclewayCounts\\bike-ped-auto-counts-2016.csv')
+    bcc2017 = pd.read_csv('Week 1\\Problem 1\\BCCCyclewayCounts\\bike-ped-auto-counts-2017.csv')
+    bcc2018 = pd.read_csv('Week 1\\Problem 1\\BCCCyclewayCounts\\bike-ped-auto-counts-2018.csv')
 
-    test = bcc2014.head()
+    bcc2014.Date = pd.to_datetime(bcc2014.Date, format='%d/%m/%Y')
+    bcc2015.Date = pd.to_datetime(bcc2015.Date, format='%d/%m/%Y')
+    bcc2016.Date = pd.to_datetime(bcc2016.Date, format='%d/%m/%Y')
+    bcc2017.Date = pd.to_datetime(bcc2017.Date, format='%d/%m/%Y')
+    bcc2018.Date = pd.to_datetime(bcc2018.Date, format='%d/%m/%Y')
 
-    print("test")
+    # Find all distinct column headers
+    all_columns = set(itertools.chain(*(
+        bcc2014.columns.values, bcc2015.columns.values,
+        bcc2016.columns.values, bcc2017.columns.values,
+        bcc2018.columns.values)))
 
+    # Find all common column headers
+    common_columns = all_columns.intersection(
+        bcc2014.columns.values, bcc2015.columns.values,
+        bcc2016.columns.values, bcc2017.columns.values,
+        bcc2018.columns.values)
+    
+    # Get common columns
+    bcc2014 = bcc2014[common_columns]
+    bcc2015 = bcc2015[common_columns]
+    bcc2016 = bcc2016[common_columns]
+    bcc2017 = bcc2017[common_columns]
+    bcc2018 = bcc2018[common_columns]
 
-load_bom()
-load_bcc()
+    # Combine vertically (Year asc)
+    bcc = pd.concat([bcc2014, bcc2015, bcc2016,
+                    bcc2017, bcc2018])
+                
+    # Drop unnecessary info
+    bcc = bcc.drop("Unnamed: 1", axis=1)
 
+    bcc.to_csv('BCC_data.csv')
+
+    return bcc
     
 
 
+bom = load_bom()
+bcc = load_bcc()
+
+combined = pd.merge(bom, bcc, on='Date')
+combined.to_csv('combined.csv')
 
 
 
