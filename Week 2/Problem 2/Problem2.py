db1a3bb0c06f87cfdedca309d611c3a35f9f2eb6
Liam Percy <liampercy123@gmail.com>
2021-04-04 20:06:30 +1000

    Problem 2 complete

--- Week 2/Problem 2/Problem2.py
+++ Week 2/Problem 2/Problem2.py
@@ -1,98 +1,150 @@
-from itertools import combinations as comb
-from sklearn.metrics import mean_squared_error, r2_score
-from datetime import datetime
-from statsmodels import api as sm
-import matplotlib.pyplot as plt
+# numpy handles pretty much anything that is a number/vector/matrix/array
 import numpy as np
+# pandas handles dataframes (exactly the same as tables in Matlab)
 import pandas as pd
+# matplotlib emulates Matlabs plotting functionality
+import matplotlib.pyplot as plt
+# stats models is a package that is going to perform the regression analysis
+from statsmodels import api as sm
+from scipy import stats
+from sklearn.metrics import mean_squared_error, r2_score
+# os allows us to manipulate variables on out local machine, such as paths and environment variables
+import os
+# self explainatory, dates and times
+from datetime import datetime, date
+# a helper package to help us iterate over objects
+import itertools
 
-
-def x2fx(x, model='quadratic'):
-    linear = np.c_[np.ones(x.shape[0]), x]
-
-    if model == 'linear':
-        return linear
-    if model == 'purequadratic':
-        return np.c_[linear, x**2]
-
-    interaction = np.vstack([x[:,i]*x[:,j] for i, j in comb(range(x.shape[1]), 2)]).T
-
-    if model == 'interactions':
-        return np.c_[linear, interaction]
-    if model == 'quadratic':
-        return np.c_[linear, interaction, x**2]
-    
-# -------------------------------------------------------------------------------------------git
+# -------------------------------------------------------------------------------------------
 # Read data
 # -------------------------------------------------------------------------------------------
 
-data = pd.read_csv('Week 2\\Problem 1\\final.csv')
-data = data.drop('Unnamed: 0', axis=1)
+combined = pd.read_csv('Week 2\\Problem 1\\final.csv')
+combined = combined.drop('Unnamed: 0', axis=1)
 
 
 # Convert strings to datetime
-data["Date"] = pd.to_datetime(data["Date"])
+combined["Date"] = pd.to_datetime(combined["Date"])
+
+train = combined[combined.Date < datetime(year=2017, month=1, day=1)]
+val = combined[((combined.Date >= datetime(year=2017, month=1, day=1)) &
+                        (combined.Date < datetime(year=2018, month=1, day=1)))]
+test = combined[((combined.Date >= datetime(year=2018, month=1, day=1)) &
+                          (combined.Date < datetime(year=2019, month=1, day=1)))]
+print('num train = {}'.format(train.shape[0]))
+print('val train = {}'.format(val.shape[0]))
+print('test train = {}'.format(test.shape[0]))
+
+
 
 # -------------------------------------------------------------------------------------------
-# Create training, validation and testing sets
+# Training simple model
 # -------------------------------------------------------------------------------------------
 
-train = data[data.Date < datetime(year=2017, month=1, day=1)]
-validation = data[((data.Date >= datetime(year=2017, month=1, day=1)) &
-                    (data.Date < datetime(year=2018, month=1, day=1)))]
-test = data[data.Date >= datetime(year=2018, month=1, day=1)]
-
-# Store BOM data headers
 X_bom = ['Rainfall amount (millimetres)',
          'Daily global solar exposure (MJ/m*m)',
          'Maximum temperature (Degree C)']
-
-# Extract inbound cyclist values as our predictors
+# want to use all variables cyclist inbound variables
 X_bcc = [x for x in train.columns.values if 'Cyclists Inbound' in x]
-# Use Bicentennial cyclists as our response
+# remove the response variable from here
 X_bcc.remove('Bicentennial Bikeway Cyclists Inbound')
-
-# Combine BCC and BOM variables
-X_variables = X_bom + X_bcc
+# combine this list of variables together by just extending the
+# BOM data with the BCC data
+X_variables_linear = X_bom + X_bcc
 Y_variable = 'Bicentennial Bikeway Cyclists Inbound'
+
 Y_train = np.array(train[Y_variable], dtype=np.float64)
-X_train = np.array(train[X_variables], dtype=np.float64)
-
-# Add constant to the model
-X_train = sm.add_constant(X_train)
-
-# Create validation and testing data
-Y_val = np.array(validation[Y_variable], dtype=np.float64)
-X_val = np.array(validation[X_variables], dtype=np.float64)
-X_val = sm.add_constant(X_val)
-
+X_train = np.array(train[X_variables_linear], dtype=np.float64)
+# want to add a constant to the model (the y-axis intercept)
+X_train_constant = sm.add_constant(X_train)
+# also creating validation data
+Y_val = np.array(val[Y_variable], dtype=np.float64)
+X_val = np.array(val[X_variables_linear], dtype=np.float64)
+X_val_constant = sm.add_constant(X_val)
+# will create our test data as well, we will need it later on
 Y_test = np.array(test[Y_variable], dtype=np.float64)
-X_test = np.array(test[X_variables], dtype=np.float64)
-X_test = sm.add_constant(X_test)
-
-# -------------------------------------------------------------------------------------------
-# Model 1 - Normal terms
-# -------------------------------------------------------------------------------------------
-
-# Create the linear model
-model = sm.OLS(Y_train, X_train)
-
-# Fit the model
+X_test = np.array(test[X_variables_linear], dtype=np.float64)
+# create the linear model
+model = sm.OLS(Y_train, X_train_constant)
+# fit the model without any regularisation
 model_1_fit = model.fit()
-pred = model_1_fit.predict(X_val)
-
-# Print results
-print('\n\nModel 1 RMSE = {}'.format(np.sqrt(mean_squared_error(Y_val, model_1_fit.predict(X_val)))))
+pred = model_1_fit.predict(X_val_constant)
+pred_train = model_1_fit.predict(X_train_constant)
+print('Linear Model Validation Data: RMSE = {}'.format(
+  np.sqrt(mean_squared_error(Y_val, pred))))
+print('Linear Model Training Data: RMSE = {}'.format(
+  np.sqrt(mean_squared_error(Y_train, pred_train))))
 print(model_1_fit.summary())
-# print(model_1_fit.params)
-
-# Plot results
+print(model_1_fit.params)
 fig, ax = plt.subplots(figsize=(8,6))
 sm.qqplot(model_1_fit.resid, ax=ax, line='s')
-plt.title('Q-Q Plot for Linear Regression Model 1')
+plt.title('Q-Q Plot for Linear Regression')
 # plt.show()
 
-# Residual plot
+
+# -------------------------------------------------------------------------------------------
+# Add quadratic terms
+# -------------------------------------------------------------------------------------------
+
+def x2fx(x, model='quadratic'):
+  """Will create a quadratic, interaction and linear
+  versions of our variable of interest
+
+  Args:
+    x (np.array):
+      Data set with columns as features (variables) that we 
+      want to generate higher order terms for
+    model (str):
+      Determine linear, interaction, quadratic, purequadratic terms
+ 
+  Returns:
+    Array with higher order terms added as additional columns
+  """
+  from itertools import combinations as comb
+  linear = np.c_[np.ones(x.shape[0]), x]
+  if model == 'linear':
+    return linear
+  if model == 'purequadratic':
+    return np.c_[linear, x**2]
+
+  interaction = np.vstack([x[:,i]*x[:,j] for i, j in
+                           comb(range(x.shape[1]), 2)]).T
+  print(interaction.shape)
+  if model == 'interaction':
+    return np.c_[linear, interaction]
+  if model == 'quadratic':
+    return np.c_[linear, interaction, x**2]
+
+
+print(X_train.shape)
+print(X_test.shape)
+X_train_complex = x2fx(X_train)
+X_val_complex = x2fx(X_val)
+X_test_complex = x2fx(X_test)
+print(X_train_complex.shape)
+print(X_val_complex.shape)
+
+
+# -------------------------------------------------------------------------------------------
+# Training complex model
+# -------------------------------------------------------------------------------------------
+model = sm.OLS(Y_train, X_train_complex)
+# fit the model without any regularisation
+model_2_fit = model.fit()
+pred_train = model_2_fit.predict(X_train_complex)
+pred = model_2_fit.predict(X_val_complex)
+print('Model 2 Training Data: Quadratic Variables RMSE = {}'.format(
+  np.sqrt(mean_squared_error(Y_train, pred_train))))
+print('Model 2 Validation Data: Quadratic Variables RMSE = {}'.format(
+  np.sqrt(mean_squared_error(Y_val, pred))))
+print(model_2_fit.summary())
+print(model_2_fit.params)
+fig, ax = plt.subplots(figsize=(8,6))
+sm.qqplot(model_2_fit.resid, ax=ax, line='s')
+plt.title('Q-Q Plot for Model 2: Quadratic Variables')
+# plt.show()
+
+# Plot residuals
 fig, ax = plt.subplots(figsize=(8,6))
 plt.scatter(pred, Y_val - pred)
 plt.title('Residual plot for unregularised model')
@@ -101,101 +153,8 @@ plt.ylabel('Residuals')
 
 
 # -------------------------------------------------------------------------------------------
-# Model 2 - Higher terms
+# Standardising data set
 # -------------------------------------------------------------------------------------------
-
-X_train_complex = x2fx(X_train)
-
-# Add constant to the model
-X_train_complex = sm.add_constant(X_train_complex)
-
-# Create validation and testing data
-X_val_complex = x2fx(X_val)
-X_val_complex = sm.add_constant(X_val_complex)
-
-X_test_complex = x2fx(X_test)
-X_test_complex = sm.add_constant(X_test_complex)
-
-# Normal shape
-print(X_train.shape)
-print(X_test.shape)
-print(X_val.shape)
-
-# Higher order terms shape
-print(X_train_complex.shape)
-print(X_test_complex.shape)
-print(X_val_complex.shape)
-
-# Create the linear model with higher order terms
-model_2 = sm.OLS(Y_train, X_train_complex)
-
-# Fit the model
-model_2_fit = model_2.fit()
-pred = model_2_fit.predict(X_val_complex)
-pred_test = model_2_fit.predict(X_test_complex)
-pred_train = model_2_fit.predict(X_train_complex)
-
-# Print results
-print('\n\nModel 2 RMSE Test = {}'.format(np.sqrt(mean_squared_error(Y_test, pred_test))))
-print('\nModel 2 RMSE Train = {}'.format(np.sqrt(mean_squared_error(Y_train, pred_train))))
-print('\nModel 2 RMSE Validation = {}'.format(np.sqrt(mean_squared_error(Y_val, pred))))
-print(model_2_fit.summary())
-# print(model_2_fit.params)
-
-# Plot results
-fig, ax = plt.subplots(figsize=(8,6))
-sm.qqplot(model_2_fit.resid, ax=ax, line='s')
-plt.title('Q-Q Plot for Linear Regression Model 2')
-# plt.show()
-
-
-
-# # -------------------------------------------------------------------------------------------
-# # Model 3 - Regularisation L1
-# # -------------------------------------------------------------------------------------------
-
-# model_3 = sm.OLS(Y_train, X_train_complex)
-# model_3_fit = model_3.fit_regularized(alpha=10, L1_wt=1)
-
-# pred = model_3_fit.predict(X_val_complex)
-
-# print('\n\nModel 3 L1 RMSE = {}'.format(np.sqrt(mean_squared_error(Y_val, pred))))
-# # print(model_3_fit.summary())
-# # print(model_3_fit.params)
-
-# resid = Y_val - pred
-# fig, ax = plt.subplots(figsize=(8,6))
-# sm.qqplot(resid, ax=ax, line='s')
-# plt.title('Q-Q Plot for Model 3: Lasso')
-# plt.show()
-
-# # -------------------------------------------------------------------------------------------
-# # Model 4 - Regularisation L2
-# # -------------------------------------------------------------------------------------------
-
-# model_4 = sm.OLS(Y_train, X_train_complex)
-# model_4_fit = model_4.fit_regularized(alpha=10, L1_wt=0)
-
-# pred = model_4_fit.predict(X_val_complex)
-# pred_test = model_4_fit.predict(X_test_complex)
-# pred_train = model_4_fit.predict(X_train_complex)
-
-# print('\n\nModel 4 RMSE Test = {}'.format(np.sqrt(mean_squared_error(Y_test, pred_test))))
-# print('\nModel 4 RMSE Train = {}'.format(np.sqrt(mean_squared_error(Y_train, pred_train))))
-# print('\nModel 4 RMSE Validation = {}'.format(np.sqrt(mean_squared_error(Y_val, pred))))
-# # print(model_4_fit.summary())
-# # print(model_4_fit.params)
-
-# resid = Y_val - pred
-# fig, ax = plt.subplots(figsize=(8,6))
-# sm.qqplot(resid, ax=ax, line='s')
-# plt.title('Q-Q Plot for Model 4: Ridge')
-# plt.show()
-
-# -------------------------------------------------------------------------------------------
-# Standardising and evaluting data
-# -------------------------------------------------------------------------------------------
-
 
 def standardise(data):
   """ Standardise/Normalise data to have zero mean and unit variance
@@ -211,6 +170,7 @@ def standardise(data):
   sigma = np.std(data, axis=0)
   scaled = (data - mu) / sigma
   return scaled, mu, sigma
+
 
 X_train_std, mu_train_x, sigma_train_x = standardise(X_train)
 Y_train_std, mu_train_y, sigma_train_y = standardise(Y_train)
@@ -230,8 +190,9 @@ X_val_complex_std = (X_val_complex - mu_train_complex_x)/sigma_train_complex_x
 X_test_complex_std = (X_test_complex - mu_train_complex_x)/sigma_train_complex_x
 
 
-
-
+# -------------------------------------------------------------------------------------------
+# Apply regression
+# -------------------------------------------------------------------------------------------
 
 def rmse(actual, pred):
   return np.sqrt(mean_squared_error(actual, pred))
@@ -245,7 +206,7 @@ def adj_r2(actual, predicted, n, p):
   adjr2 = 1 - (1 - r2) * (n - 1) / (n - p - 1);
   return adjr2
 
-def evaluate_regularisation(model, x_train, y_train, x_val, y_val, x_test, y_test,
+def evaluate_regularisation(x_train, y_train, x_val, y_val, x_test, y_test,
                             response_mu, response_sigma, alpha_list, L1_L2):
   """
   Evaluates the efficacy of regularisation for a linear model.
@@ -299,7 +260,7 @@ def evaluate_regularisation(model, x_train, y_train, x_val, y_val, x_test, y_tes
   # Ridge: L1_L2 = 0
   # Lasso: L1_L2 = 1
   # create the model
-#   model = sm.OLS(y_train, x_train)
+  model = sm.OLS(y_train, x_train)
   # initialise the value for best RMSE that is obnoxiously large, as we want this be 
   # overwritten each time RMSE is smaller, since smaller is better and we want to 
   # update our best models each time the RMSE is smaller.
@@ -367,26 +328,37 @@ def evaluate_regularisation(model, x_train, y_train, x_val, y_val, x_test, y_tes
   axs[3].set_ylabel('Residuals')
 
 
-# Evaluating simple data
+
+# -------------------------------------------------------------------------------------------
+# Ridge regression
+# -------------------------------------------------------------------------------------------
+
+# Simple data
+print('\n\nEvualting ridge regression performance on simple data')
 alpha_list = np.linspace(0, 10.0, 1000)
-model = sm.OLS(X_train_std, Y_train_std)
-evaluate_regularisation(model, X_train_std, Y_train_std, X_val_std, Y_val_std, X_test_std, Y_test_std,
+evaluate_regularisation(X_train_std, Y_train_std, X_val_std, Y_val_std, X_test_std, Y_test_std,
                         mu_train_y, sigma_train_y, alpha_list, 0)
 
-
-# Evaluating completex data
+# Complete data
+print('\n\nEvualting ridge regression performance on complex data')
 alpha_list = np.linspace(0, 10.0, 1000)
 evaluate_regularisation(X_train_complex_std, Y_train_std, X_val_complex_std, Y_val_std, X_test_complex_std, 
                         Y_test_std, mu_train_y, sigma_train_y, alpha_list, 0)
 
+# -------------------------------------------------------------------------------------------
+# Lasso regression
+# -------------------------------------------------------------------------------------------
 
-# Evaluating simple Lasso data
-alpha_list = np.linspace(0, 1.0, 1000)
+# Simple data
+print('\n\nEvualting lasso regression performance on simple data')
+alpha_list = np.linspace(0, 10.0, 1000)
 evaluate_regularisation(X_train_std, Y_train_std, X_val_std, Y_val_std, X_test_std, Y_test_std,
                         mu_train_y, sigma_train_y, alpha_list, 1)
 
-
-# Evaluating complex Lasso data
+# Complete data
+print('\n\nEvualting lasso regression performance on complex data')
 alpha_list = np.linspace(0, 10.0, 1000)
 evaluate_regularisation(X_train_complex_std, Y_train_std, X_val_complex_std, Y_val_std, X_test_complex_std, 
                         Y_test_std, mu_train_y, sigma_train_y, alpha_list, 1)
+
+plt.show()
